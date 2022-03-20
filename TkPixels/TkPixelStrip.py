b9fb18cc8aa0c7c466f355d41691f0c5d68d2d2e
TuankedeBeun <tuanke@live.nl>
2022-03-20 15:04:14 +0100

    forst commit

---
+++ TkPixels/TkPixelStrip.py
@@ -0,0 +1,32 @@
+import tkinter
+from time import sleep
+
+class TkPixel():
+    def __init__():
+        pass
+
+class TkPixelStrip():
+    def __init__(self, num_pixels, brightness = 1):
+        self.num_pixels = num_pixels
+        self.brightness = brightness
+        self.pixels = [(0, 0, 0)]*num_pixels
+        
+        self.root = ...
+        
+        self.draw()
+        
+        return [TkPixel(brightness)]*num_pixels
+    
+    def draw(self):
+        # create tkinter window with pixels
+        pass
+    
+    def show(self):
+        pass
+    
+    def fill(self, rgb):
+        pass
+    
+    
+    
+    
