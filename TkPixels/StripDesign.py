14329fa050db14a54ba984f53afdd15f8e5e3204
tuanke <tuanke@live.nl>
2024-12-14 13:16:58 +0100

    Stripped Strip class

--- TkPixels/StripDesign.py
+++ TkPixels/StripDesign.py
@@ -1,42 +1,11 @@
 from collections.abc import MutableSequence
-import tkinter as tk
-from time import sleep
-from math import sqrt
 
 class Design(MutableSequence):
-    def __init__(self, num_pixels, brightness, pixel_size = 10):
+    def __init__(self, num_pixels, brightness, canvas):
         self.num_pixels = num_pixels
         self.brightness = brightness
         self.list = [(0, 0, 0)]*num_pixels
-        
-        self.root = tk.Tk(screenName='LED strip')
-        self.width = 500 #self.root.winfo_screenwidth()
-        self.height = 750 #self.root.winfo_screenheight()
-        self.root.geometry('%dx%d+%d+%d' % (self.width, self.height, -10, 0))
-        self.canvas = tk.Canvas(master = self.root, 
-                                width = self.width, 
-                                height = self.height)
-        self.canvas.configure(background='black')
-        self.canvas.pack()
-        
-        self.pixelradius = pixel_size
-        x_bounds = (self.pixelradius, self.width - self.pixelradius)
-        y_bounds = (self.pixelradius, self.height - self.pixelradius)
-        self.coords_r = get_coordinates_of_all_leds(
-            self.num_pixels,
-            x_bounds,
-            y_bounds,
-            x_mirrored = False
-        )
-        self.coords_l = get_coordinates_of_all_leds(
-            self.num_pixels,
-            x_bounds,
-            y_bounds,
-            x_mirrored = True
-        )
-
-        self.draw()
-        
+        self.canvas = canvas
         return
     
     def __getitem__(self, i): 
@@ -80,7 +49,6 @@ class Design(MutableSequence):
         
         self.canvas.update()
         
-    
     def show(self):
         self.draw()
     
@@ -88,6 +56,3 @@ class Design(MutableSequence):
         self.check(v)
         self.list = [v]*self.num_pixels
         self.draw()
-    
-    def stop(self):
-        self.root.destroy()
