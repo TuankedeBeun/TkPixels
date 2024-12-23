3deef581e926c68a32556e2131af92c2ae1ccabd
tuanke <tuanke@live.nl>
2024-12-23 21:02:28 +0100

    Turn both LED strips off

--- ledoff.py
+++ ledoff.py
@@ -1,14 +1,18 @@
 from rpi_ws281x import *
 
 LED_COUNT = 240
-LED_PIN = 18
 LED_FREQ_HZ = 800000
-LED_DMA = 10
 LED_BRIGHTNESS = 50
 LED_INVERT = False
 LED_CHANNEL = 0
 
-strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-strip.begin()
+# Only the GPIO Pin number and the DMA channel must differ
+strip1 = Adafruit_NeoPixel(LED_COUNT, 18, LED_FREQ_HZ, 9, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip1.begin()
+strip1.show()
+del strip1
 
-strip.show()
+strip1 = Adafruit_NeoPixel(LED_COUNT, 21, LED_FREQ_HZ, 10, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip1.begin()
+strip1.show()
+del strip1
