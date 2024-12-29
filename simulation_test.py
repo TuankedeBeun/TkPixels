d7d2cfca2d6918dded1f4af66ffeb0215df48c13
tuanke <tuanke@live.nl>
2024-12-29 12:04:51 +0100

    Apply same changes to true led show

--- simulation_test.py
+++ simulation_test.py
@@ -1,7 +1,7 @@
 from TkPixels.Board import Board
 from TkPixels.Controller import Controller
 
-EFFECT_SET_NR = 0
+EFFECT_SET_NR = 1
 BPM = 174
 board = Board(1, EFFECT_SET_NR, simulate = True)
 controller = Controller(board, BPM, EFFECT_SET_NR)
