e03753d5cd6da0952fe4cb6be9a00e89618518d9
tuanke <tuanke@live.nl>
2024-12-30 22:08:52 +0100

    Always random effect set for shows

--- led_show.py
+++ led_show.py
@@ -1,9 +1,8 @@
 from TkPixels.Board import Board
 from TkPixels.Controller import Controller
 
-EFFECT_SET_NR = 0
 BPM = 120
 
 board = Board()
-controller = Controller(board, BPM, EFFECT_SET_NR)
+controller = Controller(board, BPM)
 controller.play()
