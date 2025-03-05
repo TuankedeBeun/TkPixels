b5e30b4fca9145b946efd6e1770afc9f6849ea72
tuanke <tuanke@live.nl>
2025-03-05 22:13:58 +0100

    Modify tk_show for settings

--- tk_show.py
+++ tk_show.py
@@ -3,9 +3,6 @@ from TkPixels.Controller import Controller
 from TkPixels.TkStripSequence import Strip
 import tkinter as tk
 
-EFFECT_SET_NR = 1
-BPM = 120
-
 LED_COUNT = 120
 WIDTH = 500
 HEIGHT = 750
@@ -31,5 +28,5 @@ strip_left = Strip(LED_COUNT, BRIGHTNESS, canvas, 1 + LED_COUNT)
 
 # initialize board and controller
 board = Board(strip_right, strip_left, simulate=True, canvas=canvas)
-controller = Controller(board, BPM, EFFECT_SET_NR)
+controller = Controller(board)
 controller.play()
