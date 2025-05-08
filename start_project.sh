d06e53996e42b80a1baf57ffcdc44f2a4fe2db12
TuankedeBeun <91246477+TuankedeBeun@users.noreply.github.com>
2025-05-08 22:31:34 +0200

    Put source first (#13)

--- start_project.sh
+++ start_project.sh
@@ -17,9 +17,9 @@ git pull >> logs/run.log 2>&1
 
 # start the led show
 echo "== Start show ==" >> logs/run.log
+source /home/tuanke/myenv/bin/activate
 python3 -u led_show.py >> logs/run.log 2>&1 &
 
 # start I/O
 echo "== Start I/O ==" >> logs/run.log
-source /home/tuanke/myenv/bin/activate
 python3 -u start_input.py >> logs/run.log 2>&1 &
